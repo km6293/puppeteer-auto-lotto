89c5c79f1e42a7b786cf8bf16eaca50820edbfe7
km6293 <alstn7616@naver.com>
2023-05-22 02:34:40 +0900

    팝업창 취소 및 코드 축약

--- lotto.js
+++ lotto.js
@@ -1,103 +1,55 @@
 const puppeteer = require('puppeteer');
-const axios = require('axios');
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
 (async () => {
-  try{
+  try {
+    // 스크린샷 폴더 생성
+    fs.mkdirSync('screenshot', { recursive: true });
 
-    // screenshot 폴더 생성
-    fs.readdir('screenshot', (err) => {
-      if (err) {
-          console.error('screenshot 폴더가 없어 screenshot 폴더를 생성합니다.');
-            fs.mkdirSync('screenshot');
-        }
-    });
+    // 브라우저 실행
+    const browser = await puppeteer.launch({ headless: false });
 
-
-    // launch - chrome 실행. 
-    // headless - ui를 제공 여부. false -> ui가 보여줌, true -> 백그라운드에서만 켜짐
-    const browser = await puppeteer.launch({headless: false});
-
-
-    // new Page
+    // 로그인 페이지
     const page = await browser.newPage();
-
-    // 쿠키 초기화
-    const client = await page.target().createCDPSession();
-    await client.send('Network.clearBrowserCookies');
-    await client.send('Network.clearBrowserCache');
-
-    // Move url
-    await page.goto('https://dhlottery.co.kr/user.do?method=login&returnUrl=',{
-      waitUntil: 'networkidle0'
-    });
-
-
-    // ID, PW
+    await page.goto('https://dhlottery.co.kr/user.do?method=login&returnUrl=', { waitUntil: 'networkidle0' });
     await page.waitForSelector('#userId');
-    // evaluate - Javascript 코드를 사용할 수 있게 해준다.
-    await page.evaluate((id,pw) => {
-      document.querySelector('#userId').value = id;
-      // 코드 축약
-      document.querySelector('.form input[type=password]:nth-child(2)').value = pw;
-    }, process.env.ID, process.env.PASSWORD);
-
-
-    // Login Button Click
-    await page.click(".form > a");
-
-    // 1초 기다리기.
+    await page.type('#userId', process.env.ID);
+    await page.type('.form input[type=password]:nth-child(2)', process.env.PASSWORD);
+    await page.click('.form > a');
     await page.waitForTimeout(1000);
 
+    // 구매 페이지
     const buyPage = await browser.newPage();
-                        
-    await buyPage.goto('https://ol.dhlottery.co.kr/olotto/game/game645.do',{
-      waitUntil: 'networkidle0'
-    });
+    await buyPage.goto('https://ol.dhlottery.co.kr/olotto/game/game645.do', { waitUntil: 'networkidle0' });
     await buyPage.waitForTimeout(1000);
 
-    // Buy
-    await buyPage.evaluate((val) => {
+    // 팝업 창 닫기
+    const targetPages = await browser.pages();
+    const popupPages = targetPages.filter(targetPage => targetPage !== buyPage);
+    for (const popupPage of popupPages) {
+      await popupPage.close();
+    }
+
+    // 로또 구매
+    await buyPage.evaluate((val, num) => {
       document.querySelector('#selectedTab').value = val;
-    },1);
-
-    await buyPage.waitForSelector('#btnSelectNum');
-    for(var i = 0; i < process.env.NUM; i++){
-      await buyPage.click("#btnSelectNum");
-    }
-    
-    await buyPage.click('#btnBuy')
-    await buyPage.click('#popupLayerConfirm > div > div.btns > input:nth-child(1)')
-
-    // 현재 날짜
-    Date.prototype.YYYYMMDDHHMMSS = function () {
-      var yyyy = this.getFullYear().toString();
-      var MM = pad(this.getMonth() + 1,2);
-      var dd = pad(this.getDate(), 2);
-      var hh = pad(this.getHours(), 2);
-      var mm = pad(this.getMinutes(), 2)
-      var ss = pad(this.getSeconds(), 2)
-    
-      return yyyy +  MM + dd+  hh + mm + ss;
-    };
-  
-    function pad(number, length) {
-      var str = '' + number;
-      while (str.length < length) {
-        str = '0' + str;
+      for (let i = 0; i < num; i++) {
+        document.querySelector('#btnSelectNum').click();
       }
-      return str;
-    }
-    
+    }, 1, parseInt(process.env.NUM));
+
+    await buyPage.click('#btnBuy');
+    await buyPage.click('#popupLayerConfirm > div > div.btns > input:nth-child(1)');
+
     // 스크린샷
-    await buyPage.screenshot({ path: `screenshot/${new Date().YYYYMMDDHHMMSS()}.png`, fullPage: true });
+    const timestamp = new Date().toISOString().replace(/[-:.]/g, '');
+    await buyPage.screenshot({ path: `screenshot/${timestamp}.png`, fullPage: true });
 
     // 종료
     await browser.close();
-
-  } catch(e){
+  } catch (e) {
     console.error(e);
   }
 })();
